d28504293df75f5534deef509ffdf900f9e02bc6
teramotodaiki <i101323@gmail.com>
2014-08-17 21:03:21 +0900

    メソッドを呼び出すサンプル
    戻り値void、引数なしのメソッドを呼び出すサンプルを追加。

--- Example.cs
+++ Example.cs
@@ -8,6 +8,17 @@ public class Example : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        
+        // Move object
         this.tl().MoveBy(Vector3.right*5, 0.5f).Delay(1f).MoveTo(new Vector3(0, 5, 0), 2f);
+
+        // Call method
+        this.tl().Delay(1f).Then(Hoge);
+
 	}
+
+    void Hoge()
+    {
+        Debug.Log("called");
+    }
 }
